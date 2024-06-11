0b55ca0cfbe2b7e446463657abc63dddc40346eb
cristinagiacobbe <crigiak@gmail.com>
2024-06-11 22:24:43 +0200

    Add foreign key to photos table and relation oneToMany beetwen categories and photos table

--- app/Models/Photo.php
+++ app/Models/Photo.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Photo extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'description', 'image', 'category_id'];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
--- database/migrations/2024_06_11_155022_create_evidence_table.php
+++ database/migrations/2024_06_11_155022_create_evidence_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('evidence', function (Blueprint $table) {
             $table->id();
+            $table->boolean('evidence');
             $table->timestamps();
         });
     }
